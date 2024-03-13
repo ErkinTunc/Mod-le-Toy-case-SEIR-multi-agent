03525f751e8126965e642b28d75942e0e661a972
ErkinTunc <erkintuncboya@hotmail.com>
2024-03-13 17:06:57 +0100

    modification

--- src/Human.java
+++ src/Human.java
@@ -1,5 +1,20 @@
 
 
 public class Human {
+    private String status; // S,E,I,R
+    private int elapsedTime; //time which has passed
+    private int dE;
+    private int dI;
+    private int dR;
+
+    //Constructor
+    public Human(String status,int dE, int dI, int dR){
+        this.status = status;
+        this.dE = dE;
+        this.dI = dI;
+        this.dR = dR;
+    }
+
+
     
 }
