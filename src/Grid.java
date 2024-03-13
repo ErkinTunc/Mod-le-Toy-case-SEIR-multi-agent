c9676e9368cee5d44f947d05da9e4be86d55c3b4
ErkinTunc <erkintuncboya@hotmail.com>
2024-03-13 17:08:44 +0100

    add grid class

---
+++ src/Grid.java
@@ -0,0 +1,23 @@
+
+
+public class Grid{
+    
+    private int size;
+    private Human[][] humans;
+
+    //Constructor
+    public Grid(int size){
+        this.size = size;
+        this.humans = new Human[size][size];
+    }
+
+    //Grid sınıfına, bireyleri rastgele konumlara yerleştiren bir yöntem ekleyin
+
+    //Grid sınıfına, bireylerin rastgele bir yöne hareket etmesini sağlayan bir yöntem ekleyin.
+
+    //Grid sınıfına, bireylerin enfekte olma olasılığını kontrol eden bir yöntem ekleyin.
+
+    //Grid sınıfına, bireylerin durumlarını güncelleyen bir yöntem ekleyin.
+
+
+}
